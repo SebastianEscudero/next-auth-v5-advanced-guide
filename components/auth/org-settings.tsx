e5bae450f280ae737bc1191d86c3e89d9130b411
SebastianEscudero <sebastin.escudero@uc.cl>
2024-03-27 12:10:48 -0300

    settings kinda done

--- components/auth/org-settings.tsx
+++ components/auth/org-settings.tsx
@@ -5,27 +5,19 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTransition, useState } from "react";
 import { useSession } from "next-auth/react";
+import { update } from "@/auth";
+import { db } from "@/lib/db";
 
-import { Switch } from "@/components/ui/switch";
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select";
-import { SettingsSchema } from "@/schemas";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { OrganizationSchema } from "@/schemas";
+import { DialogHeader, DialogTitle } from '@/components/ui/dialog';
 
 import { Button } from "@/components/ui/button";
-import { settings } from "@/actions/settings";
 import {
     Form,
     FormField,
     FormControl,
     FormItem,
     FormLabel,
-    FormDescription,
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -33,43 +25,49 @@ import { useCurrentUser } from "@/hooks/use-current-user";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
 import { UserRole } from "@prisma/client";
-import { useSettingsDialog } from "@/actions/use-settings-dialog";
 
-export const OrganizationSettings = () => {
-    const settingsDialog = useSettingsDialog();
+interface OrganizationSettingsProps {
+  activeOrganization: string | null;
+}
+
+export const OrganizationSettings = ({
+  activeOrganization,
+}: OrganizationSettingsProps) => {
     const user = useCurrentUser();
+
+    const activeOrg = user?.organizations.find(org => org.id === activeOrganization);
+
+    if (!activeOrg) {
+        return null;
+    }
 
     const [error, setError] = useState<string | undefined>();
     const [success, setSuccess] = useState<string | undefined>();
     const { update } = useSession();
     const [isPending, startTransition] = useTransition();
 
-    const form = useForm<z.infer<typeof SettingsSchema>>({
-        resolver: zodResolver(SettingsSchema),
+    const form = useForm<z.infer<typeof OrganizationSchema>>({
+        resolver: zodResolver(OrganizationSchema),
         defaultValues: {
-            password: undefined,
-            newPassword: undefined,
-            name: user?.name || undefined,
-            email: user?.email || undefined,
-            role: user?.role || undefined,
-            isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
+            name: activeOrg.name || undefined,
         }
     });
-
-    const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
+    
+    const onSubmit = async (values: z.infer<typeof OrganizationSchema>) => {
         startTransition(() => {
-            settings(values)
-                .then((data) => {
-                    if (data.error) {
-                        setError(data.error);
-                    }
-
-                    if (data.success) {
-                        update();
-                        setSuccess(data.success);
-                    }
-                })
-                .catch(() => setError("Something went wrong!"));
+            db.organization.update({
+                where: { id: activeOrg.id },
+                data: { name: values.name },
+            })
+            .then((data: any) => {
+                if (data) {
+                    update();
+                    setSuccess("Organization name updated successfully.");
+                }
+            })
+            .catch((error) => {
+                setError(error.message || "Something went wrong!");
+            });
         });
     }
 
@@ -89,11 +87,11 @@ export const OrganizationSettings = () => {
                   name="name"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Name</FormLabel>
+                      <FormLabel>Organization Name</FormLabel>
                       <FormControl>
                         <Input
                           {...field}
-                          placeholder="John Doe"
+                          placeholder="Sebastian's Team"
                           disabled={isPending}
                         />
                       </FormControl>
@@ -101,116 +99,6 @@ export const OrganizationSettings = () => {
                     </FormItem>
                   )}
                 />
-                {user?.isOAuth === false && (
-                  <>
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Email</FormLabel>
-                          <FormControl>
-                            <Input
-                              {...field}
-                              placeholder="john.doe@example.com"
-                              type="email"
-                              disabled={isPending}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="password"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Password</FormLabel>
-                          <FormControl>
-                            <Input
-                              {...field}
-                              placeholder="******"
-                              type="password"
-                              disabled={isPending}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="newPassword"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>New Password</FormLabel>
-                          <FormControl>
-                            <Input
-                              {...field}
-                              placeholder="******"
-                              type="password"
-                              disabled={isPending}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </>
-                )}
-                <FormField
-                  control={form.control}
-                  name="role"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Role</FormLabel>
-                      <Select
-                        disabled={isPending}
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select a role" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value={UserRole.ADMIN}>
-                            Admin
-                          </SelectItem>
-                          <SelectItem value={UserRole.USER}>
-                            User
-                          </SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {user?.isOAuth === false && (
-                  <FormField
-                    control={form.control}
-                    name="isTwoFactorEnabled"
-                    render={({ field }) => (
-                      <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-                        <div className="space-y-0.5">
-                          <FormLabel>Two Factor Authentication</FormLabel>
-                          <FormDescription>
-                            Enable two factor authentication for your account
-                          </FormDescription>
-                        </div>
-                        <FormControl>
-                          <Switch
-                            disabled={isPending}
-                            checked={field.value}
-                            onCheckedChange={field.onChange}
-                          />
-                        </FormControl>
-                      </FormItem>
-                    )}
-                  />
-                )}
               </div>
               <FormError message={error} />
               <FormSuccess message={success} />
